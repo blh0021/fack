32bb91cf8376e27d9ef97d0aded690eaed8aaf2b
Ben Harrington <ben.harrington@manheim.com>
2015-01-23 03:46:54 -0500

    fix limit to directory depth

--- fack.gemspec
+++ fack.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name        = 'fack'
-  s.version     = '0.0.0'
+  s.version     = '0.0.1'
   s.date        = '2014-01-22'
   s.summary     = "Fack"
   s.description = "Fack is a simple gem for finding files within a path."
   s.authors     = ["Ben Harrington"]
   s.email       = 'ben@harringtonweb.com'
   s.files       = ["lib/fack.rb"]
-  s.homepage    = 'http://www.benharrington.com'
+  s.homepage    = 'https://github.com/blh0021/fack'
   s.license     = 'MIT'
 
   s.files       = ["lib/fack.rb", "bin/fack"]
--- lib/fack.rb
+++ lib/fack.rb
@@ -2,13 +2,24 @@ require 'find'
 
 class Fack
   def help
-    puts "to use fack path"
+    puts "To use: fack <search_string>"
   end
 
   def find_file(start, file)
     Dir.entries(start)[2..-1].each do |d|
       puts d if d.include?(file)
       find_file(d, file) if File.directory?(d)
+    end
+  end
+
+  def print_find(start, file)
+    stack = Dir.entries(start)[2..-1]
+    while (!stack.empty?)
+      f = stack.pop
+      puts f if f.include?(file)
+      if File.directory?(f)
+        stack = stack + Dir.entries(f)[2..-1]
+      end
     end
   end
 end
