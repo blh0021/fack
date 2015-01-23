08847159c236b6e9806d5f2077ae69275e788842
Ben Harrington <ben.harrington@manheim.com>
2015-01-23 04:38:36 -0500

    refactor directory find

--- lib/fack.rb
+++ lib/fack.rb
@@ -5,21 +5,11 @@ class Fack
     puts "To use: fack <search_string>"
   end
 
-  def find_file(start, file)
-    Dir.entries(start)[2..-1].each do |d|
-      puts d if d.include?(file)
-      find_file(d, file) if File.directory?(d)
-    end
+  def print_check(a, b)
+    puts a if a.include? b
   end
 
   def print_find(start, file)
-    stack = Dir.entries(start)[2..-1]
-    while (!stack.empty?)
-      f = stack.pop
-      puts f if f.include?(file)
-      if File.directory?(f)
-        stack = stack + Dir.entries(f)[2..-1]
-      end
-    end
+    Dir.glob('**/*') { |f| print_check(f, file) }
   end
 end
