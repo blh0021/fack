654fa896ba73e51dd4fb531d22647e7bb8a4a421
Ben Harrington <ben.harrington@manheim.com>
2015-02-18 07:14:01 -0500

    Added Ignore Flag

--- fack.gemspec
+++ fack.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'fack'
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.date        = Time.now.strftime("%Y-%m-%d")
   s.summary     = "Fack"
   s.description = "Fack is a simple gem for finding files within a path."
--- lib/fack.rb
+++ lib/fack.rb
@@ -1,13 +1,25 @@
 require 'colorize'
 
 class Fack
+
+  attr_accessor :ignore
+
   def help
     puts "To use: fack <search_string>"
   end
 
   def print_check(a, b)
-    if a.include? b
-      puts a.gsub(/#{b}/, b.colorize(:red))
+    search=nil
+    if @ignore
+      search = a.to_enum(:scan, /#{b}/i).map { Regexp.last_match }
+        #a.match(/#{b}/i)
+    else
+      search = a.to_enum(:scan, /#{b}/).map { Regexp.last_match }
+        #match(/#{b}/)
+    end
+
+    search.each do |s|
+      puts a.gsub(/#{s.to_s}/, s.to_s.colorize(:red))
     end
   end
 
